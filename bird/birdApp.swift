d2c8c705ef01d8d7468ba0e45a80251c569bff5a
Ahmed Shawky <59096080+AhmedShawkyAhmed@users.noreply.github.com>
2025-03-01 22:30:46 +0200

    init

---
+++ bird/birdApp.swift
@@ -0,0 +1,17 @@
+//
+//  birdApp.swift
+//  bird
+//
+//  Created by Ahmed Shawky on 01/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct birdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ birdTests/birdTests.swift
@@ -0,0 +1,17 @@
+//
+//  birdTests.swift
+//  birdTests
+//
+//  Created by Ahmed Shawky on 01/03/2025.
+//
+
+import Testing
+@testable import bird
+
+struct birdTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
